7d6e634629bcda4dc138f54e953d1317a912e6f2
Vbinnie <vinicarvallhoyt@gmail.com>
2025-05-07 16:11:07 -0400

    dexflex_yt as thick v10

--- src/main/java/net/vbinnie/voracity/datagen/ModRecipeProvider.java
+++ src/main/java/net/vbinnie/voracity/datagen/ModRecipeProvider.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.block.Blocks;
 import net.minecraft.data.server.recipe.RecipeJsonBuilder;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
@@ -28,54 +29,6 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     @Override
     public void generate(Consumer<RecipeJsonProvider> recipeExporter) {
-
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.NETHERGOLD_BLOCK)
-                .pattern("RRR")
-                .pattern("RRR")
-                .pattern("RRR")
-                .input('R', ModItems.NETHERGOLD_INGOT)
-                .criterion(hasItem(ModItems.NETHERGOLD_INGOT), conditionsFromItem(ModItems.NETHERGOLD_INGOT))
-                .offerTo(recipeExporter);
-
-
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.END_MINERAL_BLOCK)
-                .pattern("RRR")
-                .pattern("RRR")
-                .pattern("RRR")
-                .input('R', ModItems.END_MINERAL)
-                .criterion(hasItem(ModItems.END_MINERAL), conditionsFromItem(ModItems.END_MINERAL))
-                .offerTo(recipeExporter);
-
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.FILTHY_PIG_EYE)
-                .pattern(" G ")
-                .pattern("GHG")
-                .pattern(" G ")
-                .input('G', Items.GOLD_INGOT)
-                .input('H', ModItems.HOGLIN_EYE)
-                .criterion(hasItem(Items.GOLD_INGOT), conditionsFromItem(Items.GOLD_INGOT))
-                .offerTo(recipeExporter);
-
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.NETHERGOLD_INGOT, 2)
-                .pattern("NFG")
-                .input('G', Items.GOLD_INGOT)
-                .input('F', ModItems.FILTHY_PIG_EYE)
-                .input('N', Items.NETHERITE_SCRAP)
-                .criterion(hasItem(ModItems.NETHERGOLD_INGOT), conditionsFromItem(ModItems.NETHERGOLD_INGOT))
-                .offerTo(recipeExporter, new Identifier(getRecipeName(ModItems.NETHERGOLD_INGOT) + "_recipe_1"));
-
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.NETHERGOLD_INGOT, 2)
-                .pattern("GFN")
-                .input('G', Items.GOLD_INGOT)
-                .input('F', ModItems.FILTHY_PIG_EYE)
-                .input('N', Items.NETHERITE_SCRAP)
-                .criterion(hasItem(ModItems.NETHERGOLD_INGOT), conditionsFromItem(ModItems.NETHERGOLD_INGOT))
-                .offerTo(recipeExporter, new Identifier(getRecipeName(ModItems.NETHERGOLD_INGOT) + "_recipe_1_alt"));
-
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.NETHERGOLD_INGOT, 9)
-                .input(ModBlocks.NETHERGOLD_BLOCK)
-                .criterion(hasItem(ModBlocks.NETHERGOLD_BLOCK), conditionsFromItem(ModBlocks.NETHERGOLD_BLOCK))
-                .offerTo(recipeExporter, new Identifier(getRecipeName(ModItems.NETHERGOLD_INGOT) + "_recipe2"));
-
 
     }
 }
